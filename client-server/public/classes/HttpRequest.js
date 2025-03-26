d65cf95d0a01f7d1ad3cc04e3ca7f5bf33dc0c83
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-03-26 10:15:47 -0300

    feat: fetch

--- client-server/public/classes/HttpRequest.js
+++ client-server/public/classes/HttpRequest.js
@@ -1,4 +1,3 @@
-const { application } = require("express");
 
 class HttpRequest {
 
--- client-server/public/models/User.js
+++ client-server/public/models/User.js
@@ -80,7 +80,7 @@ class User {
 
     static getUsersStorage() {
 
-        return HttpRequest.get('/users');
+        return Fetch.get('/users');
 
     }
 
@@ -105,11 +105,11 @@ class User {
 
        if (this.id){
 
-           promise =  HttpRequest.put(`/users/${this.id}`, this.toJSON());
+           promise =  Fetch.put(`/users/${this.id}`, this.toJSON());
 
        } else {
 
-           promise = HttpRequest.post(`/users/`, this.toJSON());
+           promise = Fetch.post(`/users/`, this.toJSON());
 
        }
 
@@ -133,7 +133,7 @@ class User {
 
     remove(){
 
-       return HttpRequest.delete(`/users/${this.id}`);
+       return Fetch.delete(`/users/${this.id}`);
 
     }
 
